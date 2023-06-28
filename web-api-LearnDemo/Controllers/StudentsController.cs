27f5b696885c59d3ecf628dcd0e87023645a7d81
Dhruv Rathod <dhruvrathod3110@gmail.com>
2023-06-28 23:57:06 +0530

    Add project files.

---
+++ web-api-LearnDemo/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_api_LearnDemo.Controllers
+{
+
+    // https://localhost:portnumber/api/Students
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET:https://localhost:portnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] StudentsName = new string[]
+            {
+                 "Dhruv",
+                 "Jenil",
+                 "Bhargav",
+                 "Dhruvil",
+            };
+            return Ok(StudentsName);    
+        }
+    }
+}
---
+++ web-api-LearnDemo/Models/Domain/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace web_api_LearnDemo.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; } 
+        public string Name { get; set; }
+    }
+}
