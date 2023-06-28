b98d40432deef7ebb5b04279614937dfe2f16e30
Dhruv Rathod <dhruvrathod3110@gmail.com>
2023-06-29 00:30:39 +0530

    Creat DbContext and Dbset ; ( Migration  & Update databse ) - completed

---
+++ web-api-LearnDemo/Data/StudentWebApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_api_LearnDemo.Models.Domain;
+
+namespace web_api_LearnDemo.Data
+{
+    public class StudentWebApiDbContext : DbContext
+    {
+        public StudentWebApiDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+                
+        }
+
+        public DbSet<Difficulty> Diffculties { get; set; }  
+        public DbSet<Region> Regions { get; set; }   
+        public DbSet<Walk> Walks { get; set; }  
+    }
+}
--- web-api-LearnDemo/Program.cs
+++ web-api-LearnDemo/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using web_api_LearnDemo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<StudentWebApiDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString")));
 
 var app = builder.Build();
 
