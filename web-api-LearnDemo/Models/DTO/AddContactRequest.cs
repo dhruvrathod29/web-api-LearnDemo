8021d8995f7e1daa2288638e6b093a506458cf58
Dhruv Rathod <dhruvrathod3110@gmail.com>
2023-06-29 19:22:06 +0530

    'Completed - Contact ( WebApi, EFCore, MSSQL)'

--- web-api-LearnDemo/Models/DTO/AddContactRequest.cs
+++ web-api-LearnDemo/Models/DTO/AddContactRequest.cs
@@ -2,7 +2,7 @@
 {
     public class AddContactRequest
     {
-        public string Name { get; set; }
+        public string Name { get; set; } 
         public string Email { get; set; }
         public long Phone { get; set; }
         public string Address { get; set; }
